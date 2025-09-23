2b1f29b3cdc26dad12b50c5c396a42b05fb0f00e
Mikkel Malmberg <mikkel@brnbw.com>
2025-09-23 13:52:30 +0200

    Apply Prettier

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 const ref = require('pear-ref')
-module.exports = function info (link, opts) {
+module.exports = function info(link, opts) {
   const ipc = global.Pear?.[global.Pear?.constructor.IPC]
   if (!ipc) throw new Error('pear-info is designed for Pear - IPC missing')
   ref.ref()
